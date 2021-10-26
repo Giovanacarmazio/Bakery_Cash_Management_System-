d0e28ce427c40df3662817e2a0609476a50eb5bc
Giovana Carmazio <83242981+Giovanacarmazio@users.noreply.github.com>
2021-10-26 13:34:16 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,5 +1,5 @@
-#Criei um sistema de uma clinica veterinaria em JAVA, e confesso que esse em PYTHON foi um desafio mesmo rsrs não achei uma melhor forma de fazer
-#sem um milhão de loopins for a menos a sua
+import operacoes as op
+
 
 opcao_selecionada = None
 valor_total = 0
@@ -11,26 +11,23 @@ while True:
                                         1- RETIRAR ITEM
                                         2- CONSULTAR TOTAL
                                         3- PAGAMENTO
-                                        4- DESLIGAR
                                         """))
-    elif opcao_selecionada not in range(5):
+    elif opcao_selecionada not in range(4):
         print("Opção inválida, tente novamente")
         opcao_selecionada = None
     elif opcao_selecionada == 0:
         nome_item = input("Insira o nome do item")
         valor_unitario = float(input("Insira o valor unitario"))
         quantidade = int(input("Insira a quantidade"))
-        total_item = valor_unitario*quantidade
-        valor_total = valor_total + total_item
+        valor_total = op.adicionar_item(nome_item,valor_unitario,quantidade, valor_total)
         print(f"O item {nome_item} foi adicionado com sucesso!")
         opcao_selecionada = None
     elif opcao_selecionada == 1:
         nome_item = input("Insira o nome do item")
         valor_unitario = float(input("Insira o valor unitario"))
         quantidade = int(input("Insira a quantidade"))
-        total_item = valor_unitario*quantidade
-        resultado = valor_total - total_item
-        if resultado < 0:
+        resultado = op.retirar_item(nome_item, valor_unitario, quantidade, valor_total)
+        if resultado == "error!":
             print(f"Valor inválido, não pode ser menor que {valor_total}")
             opcao_selecionada == None
         else:
@@ -38,23 +35,9 @@ while True:
             print(f"O item {nome_item} foi retirado com sucesso!")
         opcao_selecionada = None
     elif opcao_selecionada == 2:
-        print(f"O total é de: R${valor_total}")
+        print(op.consultar_total(valor_total))
         opcao_selecionada = None
     elif opcao_selecionada == 3:
         forma_pagamento = int(input("Qual a forma de pagamento? (0 - Dinheiro) (1 - Cartão)"))
-        if forma_pagamento == 0:
-            valor_total = valor_total*0.9
-            pagamento = float(input("Qual o valor do pagamento?"))
-            troco = pagamento - valor_total
-            if troco < 0:
-                print(f"O pagamento deve ser maior que o valor total!")
-                opcao_selecionada = None
-            else:
-                print(f"Pagamento realizado! O troco é de {troco}")
-        elif forma_pagamento == 1:
-            print(f"Pagamento realizado!")
-        else:
-            print(f"Opção inválida!")
-        opcao_selecionada = None 
-    elif opcao_selecionada == 4:
-        break
+        print(op.pagamento(valor_total,forma_pagamento))
+        opcao_selecionada = None
